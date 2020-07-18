ae6f3d75eb817570e63e1807d336bc6a03854e8e
Tristan Seifert <tristan@tseifert.me>
2020-07-18 01:54:50 -0500

    Added main window split view tracking item

--- app_macos/src/Main Window/MainWindowViewController.swift
+++ app_macos/src/Main Window/MainWindowViewController.swift
@@ -9,7 +9,7 @@ import Cocoa
 
 import CocoaLumberjackSwift
 
-class MainWindowViewController: NSSplitViewController, NSMenuItemValidation {
+class MainWindowViewController: NSSplitViewController, NSMenuItemValidation, NSToolbarDelegate {
     /// Filter for the sidebar selection
     @objc dynamic var sidebarFilter: NSPredicate?
     
@@ -28,20 +28,29 @@ class MainWindowViewController: NSSplitViewController, NSMenuItemValidation {
                   withKeyPath: #keyPath(SidebarController.filter),
                   options: nil)
         
-//        if let window = self.view.window, let toolbar = window.toolbar {
-//            if !toolbar.items.contains(where: { $0.itemIdentifier == .sidebarTrackingSeparatorItemIdentifier }) {//
-//                var items = toolbar.items
-//
-//                // add the separator
-//                let s = NSTrackingSeparatorToolbarItem(identifier: .sidebarTrackingSeparatorItemIdentifier,
-//                                                       splitView: self.splitView,
-//                                                       dividerIndex: 1)
-//                items.insert(s, at: 0)
-//
-//                // restore toolbar items
-//                toolbar.items = items
-//            }
-//        }
+        if let window = self.view.window, let toolbar = window.toolbar {
+            if !toolbar.items.contains(where: { $0.itemIdentifier == .sidebarTrackingSeparatorItemIdentifier }) {
+                toolbar.delegate = self
+                toolbar.insertItem(withItemIdentifier: .sidebarTrackingSeparatorItemIdentifier, at: 1)
+            }
+        }
+    }
+    
+    // MARK: - Toolbar delegate
+    /**
+     * Creates toolbar items.
+     *
+     * In this implementation, we only create the sidebar tracking space item.
+     */
+    func toolbar(_ toolbar: NSToolbar, itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier, willBeInsertedIntoToolbar flag: Bool) -> NSToolbarItem? {
+        if itemIdentifier == .sidebarTrackingSeparatorItemIdentifier {
+            return NSTrackingSeparatorToolbarItem(identifier: .sidebarTrackingSeparatorItemIdentifier,
+                                                  splitView: self.splitView,
+                                                  dividerIndex: 0)
+        }
+        
+        // not supported
+        return nil
     }
     
     // MARK: - Child message handling
